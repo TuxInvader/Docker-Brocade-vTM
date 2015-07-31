2586f1706960a6ad6518a91a469b8cf22995c732
Mark Boddington <dev@badpenguin.co.uk>
2015-07-31 11:14:47 +0100

    Default to generating less complex passwords

--- runzeus.sh
+++ runzeus.sh
@@ -8,17 +8,19 @@ plog() {
 }
 
 genPasswd() {
-   if [ "$ZEUS_PASS" == "RANDOM" ] || [ "$ZEUS_PASS" == "SIMPLE" ]
+   if [ "$ZEUS_PASS" == "RANDOM" ] || [ "$ZEUS_PASS" == "SIMPLE" ] || [ "$ZEUS_PASS" == "STRONG" ]
    then
-      chars=( a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 0 \
-              A B C D E F G H I J K L M N O P Q R S T U V W X Y Z , \. \< \> \~ \# \[ \] \
-              \- \= \+ \_ \* \& \^ \% \$ \; \: \( \) )
 
-		# Simpler passwords
-		if [ "$ZEUS_PASS" == "SIMPLE" ]
+		# Default for RANDOM/SIMPLE is alphanumeric with , . - + _ 
+		chars=( a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 0 \
+			     A B C D E F G H I J K L M N O P Q R S T U V W X Y Z , \. \- \+ \_ )
+
+		# Use Extra Strong Passwords (more symbols)
+		if [ "$ZEUS_PASS" == "STRONG" ]
 		then
-			chars=( a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 0 \
-					A B C D E F G H I J K L M N O P Q R S T U V W X Y Z , \. \- \+ \_ )
+      	chars=( a b c d e f g h i j k l m n o p q r s t u v w x y z 1 2 3 4 5 6 7 8 9 0 \
+                 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z , \. \< \> \~ \# \[ \] \
+                 \- \= \+ \_ \* \& \^ \% \$ \; \: \( \) )
 		fi
 
       length=$(( 9 + $(( $RANDOM % 3 )) ))
